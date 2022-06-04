75a313def3d51c09e26a3fbb5bc9a6614871613e
dkbarrett <dk.barrett+gh@hotmail.com>
2022-06-05 01:18:43 +1200

    Add error and success messages, fix js

---
+++ assets/js/contact.js
@@ -0,0 +1,49 @@
+$(function() {
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      $("#sendMessageButton").prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+
+      var formData = $form.serialize()
+      var url = $("#contactForm").attr("action");
+
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        cache: false,
+
+		    success: function() {
+          $('#email-success').show()
+          $('#contactForm').trigger("reset");
+        },
+
+        error: function() {
+          $('#email-error').show()
+          $('#contactForm').trigger("reset");
+        },
+
+        complete: function() {
+          setTimeout(function() {
+            $("#sendMessageButton").prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on form hide fail/success boxes */
+$('#name').on('focus', function() {
+  $('#email-success').hide();
+  $('#email-error').hide();
+});
